6513ee39167f44233c53eec0a42098822e2e7c80
Eric Vinicius Basso <eric.viniciusbasso@gmail.com>
2023-11-09 16:54:54 -0300

    Adicionado a guia login

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -4,10 +4,12 @@ import { HomeComponent } from "./login/navbar/home/home.component";
 import { SobreComponent } from "./login/navbar/sobre/sobre.component";
 import { ContatoComponent } from "./login/navbar/contato/contato.component";
 import { LocalizacaoComponent } from "./login/navbar/localizacao/localizacao.component";
+import { LoginComponent } from "src/app/login/login.component";
 
 export const AppRoutes: Routes = [
     { path: 'home', component: HomeComponent },
     { path: 'sobre', component: SobreComponent },
     { path: 'contato', component: ContatoComponent },
-    { path: 'localizacoes', component: LocalizacaoComponent }
+    { path: 'unidades', component: LocalizacaoComponent },
+    { path: 'login', component: LoginComponent }
   ]
