1f17c53bc67901a65b3fa7b9fedfa9615bb62775
Eric Basso <eric.viniciusbasso@gmail.com>
2023-11-12 17:20:27 -0300

    login - ver senha

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
+  tipoDoInput:string 
+  
+  VerSenha(){
+    if(this.tipoDoInput == 'password'){
+      this.tipoDoInput = 'text';
+    }
+  }
+
+  EsconderSenha(){
+    if(this.tipoDoInput == 'text'){
+      this.tipoDoInput = 'password';
+    }
+  }
+
+  ngOnInit(): void {
+    this.tipoDoInput = 'password';
+  }
 }
